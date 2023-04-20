18250344b95287ff9d9877022729c2899b157b86
soup <soup@r4tio.dev>
2023-04-20 22:34:12 +0200

    add delete option

--- hardlinksearch.py
+++ hardlinksearch.py
@@ -26,9 +26,21 @@ def save_to_csv(non_hardlinked_files, csv_file_path):
         for file_path in non_hardlinked_files:
             csv_writer.writerow([file_path])
 
+def delete_files(non_hardlinked_files, dry_run):
+    for file_path in non_hardlinked_files:
+        folder_path = os.path.dirname(file_path)
+        if dry_run:
+            print(f"[DRY-RUN] Would delete all files in folder: {folder_path}")
+        else:
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    file_to_delete = os.path.join(root, file)
+                    os.remove(file_to_delete)
+                    print(f"Deleted file: {file_to_delete}")
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python3 hardlinksearch.py /path/to/dir")
+        print("Usage: python3 hardlinksearch.py /path/to/dir [--delete [--dry-run]]")
         sys.exit(1)
 
     dir_path = sys.argv[1]
@@ -36,3 +48,7 @@ if __name__ == "__main__":
 
     non_hardlinked_files = get_non_hardlinked_files(dir_path)
     save_to_csv(non_hardlinked_files, csv_file_path)
+
+    if len(sys.argv) > 2 and sys.argv[2] == '--delete':
+        dry_run = len(sys.argv) > 3 and sys.argv[3] == '--dry-run'
+        delete_files(non_hardlinked_files, dry_run)
