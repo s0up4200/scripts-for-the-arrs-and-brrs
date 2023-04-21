bc1b4068ee2a924b5ee6d679fa6f3a0e05d1368b
soup <soup@r4tio.dev>
2023-04-21 17:29:18 +0200

    removed duplication

--- hardlink-radarr.py
+++ hardlink-radarr.py
@@ -79,7 +79,7 @@ def monitor_and_search_movie(movie_id, movie_file_path):
     # Delete the movie file
     try:
         os.remove(movie_file_path)
-        print(f"Deleted movie file: {movie_file_path}")
+        print(f"Deleted non-hardlinked movie file: {movie_file_path}")
     except Exception as e:
         print(f"Error deleting movie file: {e}")
         return
@@ -97,49 +97,12 @@ def monitor_and_search_movie(movie_id, movie_file_path):
 
     search_url = f"{RADARR_URL}/api/v3/command"
     search_payload = {"name": "MoviesSearch", "movieIds": [movie_id]}
-
+    
     response = requests.post(search_url, json=search_payload, params={"apikey": RADARR_API_KEY})
     #print(f"Search movie response status code: {response.status_code}")
     #print(f"Search movie response text: {response.text}")
 
     response.raise_for_status()
-
-def process_movies(non_hardlinked_files, amount):
-    for movie_file_path in non_hardlinked_files[:amount]:
-        folder_path = os.path.dirname(movie_file_path)
-        movie = get_movie_by_folder_path(folder_path)
-
-        if movie:
-            print(f"Searching for movie: {movie['title']} (ID: {movie['id']})")
-            monitor_and_search_movie(movie['id'], movie_file_path)
-            non_hardlinked_files.remove(movie_file_path)
-            with open("non_hardlinked_files.csv", "w") as f:
-                f.write("File Path\n")
-                for remaining_file in non_hardlinked_files:
-                    f.write(f"{remaining_file}\n")
-        else:
-            print(f"Movie not found in Radarr for folder path: {folder_path}")
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 hardlink-radarr.py /path/to/dir [--replace <amount>]")
-        sys.exit(1)
-
-    dir_path = sys.argv[1]
-    csv_file_path = 'non_hardlinked_files.csv'
-
-    non_hardlinked_files = get_non_hardlinked_files(dir_path)
-    save_to_csv(non_hardlinked_files, csv_file_path)
-
-    if len(sys.argv) > 2 and sys.argv[2] == '--replace':
-        if len(sys.argv) < 4:
-            print("Error: Missing amount. Usage: python3 hardlink-radarr.py /path/to/dir --replace <amount>")
-            sys.exit(1)
-
-        amount = int(sys.argv[3])
-        non_hardlinked_files = read_from_csv(csv_file_path)
-        process_movies(non_hardlinked_files, amount)
-
 
 def process_movies(non_hardlinked_files, amount):
     for movie_file_path in non_hardlinked_files[:amount]:
